7492ff9630cec99894306b87c3114be43828f4a2
shborg <al@shebang.org>
2023-05-21 16:16:38 +0200

    fix: fixing bug in display func

--- lua/telescope/_extensions/query-tools/make_entry.lua
+++ lua/telescope/_extensions/query-tools/make_entry.lua
@@ -1,9 +1,10 @@
+local fs_stat              = require "telescope._extensions.file_browser.fs_stat"
 local entry_display        = require "telescope.pickers.entry_display"
 local Path                 = require "plenary.path"
 
 local make_entry           = {}
 local set_default_entry_mt = require("telescope.make_entry").set_default_entry_mt
--- local kinds = require 'lvm.query-tools'.kinds()
+local kinds = require 'lvm.query-tools'.kinds()
 
 make_entry.gen_from_query = function(kind)
   return function(opts)
@@ -31,11 +32,12 @@ make_entry.gen_from_query = function(kind)
 
       local function make_display(entry)
 
+        local icon_display = kinds[entry.kind] or "NI"
+
         return displayer(
           {
 
-            "icon",
-            { string.format("[%s]", entry.index), "Title" },
+            {icon_display, "String"},
             { string.format("[%s]", entry.index), "Title" },
 
             entry.name,
@@ -49,6 +51,7 @@ make_entry.gen_from_query = function(kind)
 
       return set_default_entry_mt({
         display = make_display,
+        kind = entry.kind,
         name = entry.name,
         filename = entry.path,
         value = entry.path,
